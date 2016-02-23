04a4636737d5f5536edd0125e13534e3bc3bfa3a
Stefan <stefanm1010@gmail.com>
2016-02-23 16:42:11 -0500

    Modified function call

--- Ultrasonic.c
+++ Ultrasonic.c
@@ -15,7 +15,7 @@ task checkWall(threshold){
 		float dist=sensorValue(s1);
 		motor(motorA)=-100; //full reverse
 		motor(motorB)=-100;
-		rudder.move(-45); //pseudocode, we should have a rudder and elevator_object
+		moveRudder(-45); //pseudocode, we should have a rudder and elevator function
 		wait1msc(200);
 		if (sensorValue(s1)>dist)//check to make sure that we are turning away from wall
 			rudder.move(45)
@@ -27,16 +27,15 @@ else return false;
 task land(){
 	motor(motorA)=0;
 	motor(motorB)=0;
-	elevator.move(-45);//angle elevators to descend
+	moveElevator(-45);//angle elevators to descend
 	wait1msc(3000);
-	elevator.move(0);
+	moveElevator(0);
 	while (sensorValue(s2)<30){}
-	elevator.move(30)//level out
+	moveElevator(30);//level out
 	motor(motorA)=-30;
 	motor(motorB)=-30; // slow down
 	wait1msc(500);
-	elevator.move(0);
+	moveElevator(0);
 	while sensorValue(s2)>=20{}
 	displayString(0,"Landed!");
 }
-
